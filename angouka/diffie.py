c72f928a3cbdb1bceb846ebab7aa3ede9954c6d8
Derek <sjukledighet@gmail.com>
2018-07-22 23:28:18 -0400

    Add encryption to Diffie class

--- angouka/diffie.py
+++ angouka/diffie.py
@@ -1,16 +1,32 @@
+import hashlib
+import base64
+import random
+
+from cryptography.fernet import Fernet
+
+
 class Diffie(object):
-    def __init__(self, n, g=2, p=101):
-        self.n = n              # private number
-        self.g = g              # generator
-        if self.is_prime(p):
-            self.p = p          # prime modulus
+    def __init__(self, n=None, g=2, p=101):
+        if n is None:
+            self.n = random.getrandbits(32)
         else:
-            raise ValueError('Prime modulus must be prime')
+            self.n = n          # private number
+            
+        self.g = g              # generator
+        
+        # if self.is_prime(p):
+        self.p = p          # prime modulus
+        # else:
+        #     raise ValueError('Prime modulus must be prime')
+        
         self.pub = self.discrete_log(self.g, self.n, self.p)
         self.priv = None
 
     def __call__(self, pub):
         self.priv = self.discrete_log(pub, self.n, self.p)
+        self.priv_hash = hashlib.md5(str(self.priv)).hexdigest()
+        self.priv_b64 = base64.urlsafe_b64encode(self.priv_hash)
+        self.priv_key = Fernet(self.priv_b64)
 
     def __str__(self):
         return '({}, {})'.format(self.pub, self.priv)
@@ -21,9 +37,9 @@ class Diffie(object):
     def discrete_log(self, g, n, p):
         return pow(g, n, p)
 
-    @staticmethod
-    def is_prime(n):
-        for i in xrange(2, int(n**.5)+1):
-            if n % i == 0:
-                return False
-        return True
+    # @staticmethod
+    # def is_prime(n):
+    #     for i in xrange(2, int(n**.5)+1):
+    #         if n % i == 0:
+    #             return False
+    #     return True
